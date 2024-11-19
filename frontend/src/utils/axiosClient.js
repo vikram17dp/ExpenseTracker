582c55689aa6865d151513eb49cce688953b5dfa
vikram17dp <vikramdp505@gmail.com>
2024-11-19 21:05:05 +0530

    adding the axios url

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const axiosClient = axios.create({
-    baseURL : 'https://expensetracker-4rn9.onrender.com'
+    baseURL : 'https://expensetracker-unfx.onrender.com'
 })
